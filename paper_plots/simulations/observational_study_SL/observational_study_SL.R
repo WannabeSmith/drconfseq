eb75abcf23597bdbaceb48b447f3e559fd3f57e4
Ian Waudby-Smith <iwaudbysmith@gmail.com>
2021-03-09 16:36:12 -0500

    Update sims

--- paper_plots/simulations/observational_study_SL/observational_study_SL.R
+++ paper_plots/simulations/observational_study_SL/observational_study_SL.R
@@ -48,43 +48,13 @@ pi_fn <- get_SL_fn(family = binomial)
 glm_reg_1 = get_SL_fn(SL.library = "SL.glm")
 
 
-times <- unique(round(logseq(250, 10000, n = 30)))
+times <- unique(round(logseq(250, n, n = 30)))
 alpha <- 0.05
 n_cores <- detectCores()
 
 # Split the sample (if we don't do this explicitly,
 # confseq_ate or drate_variables_sequential can automatically)
 train_idx <- rbinom(n, p = 0.5, size = 1) == 1
-
-# # Doing this the more abstract way so that we can plot confidence
-# # sequences alongside confidence intervals without re-doing the analysis.
-# # In practice this is simpler by just using confseq_ate.
-# drate_variables_SL_list <-
-#   drate_variables_sequential(y = y, X = X,
-#                              treatment = treatment,
-#                              regression_fn_1 = sl_reg_1,
-#                              propensity_score_fn = function(y, X, newX){1/2},
-#                              train_idx = train_idx,
-#                              times = times,
-#                              n_cores = n_cores)
-#
-# drate_variables_glm_list <-
-#   drate_variables_sequential(y = y, X = X,
-#                              treatment = treatment,
-#                              regression_fn_1 = glm_reg_1,
-#                              propensity_score_fn = function(y, X, newX){1/2},
-#                              train_idx = train_idx,
-#                              times = times,
-#                              n_cores = n_cores)
-#
-# drate_variables_unadj_list <-
-#   drate_variables_sequential(y = y, X = X,
-#                              treatment = treatment,
-#                              regression_fn_1 = function(y, X, newX){0},
-#                              propensity_score_fn = function(y, X, newX){1/2},
-#                              train_idx = train_idx,
-#                              times = times,
-#                              n_cores = n_cores)
 
 confseq_SL <- confseq_ate(y, X, treatment, regression_fn_1 = sl_reg_1,
                           regression_fn_0 = sl_reg_1,
@@ -97,15 +67,9 @@ confseq_glm <- confseq_ate(y, X, treatment, regression_fn_1 = glm_reg_1,
                                                            family=binomial()),
                            train_idx = train_idx, t_opt = 250, alpha=alpha,
                            times=times, n_cores = n_cores, cross_fit = TRUE)
-# confseq_unadj <- confseq_ate(y, X, treatment,
-#                              regression_fn_1 = function(y, X, newX){0},
-#                              propensity_score_fn = function(y, X, newX){1/2},
-#                              train_idx = train_idx, t_opt = 250, alpha=alpha,
-#                              times=times, n_cores = n_cores)
-
 
 confseq_unadj <- confseq_ate_unadjusted(y = y, treatment = treatment,
-                                        propensity_score = 1/2,
+                                        propensity_score = mean(treatment),
                                         t_opt = 250, alpha = alpha,
                                         times = times)
 
