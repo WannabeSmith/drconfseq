ee6afe884a5cc27690e947b5aec739230f2f9c4b
Ian Waudby-Smith <5490890+WannabeSmith@users.noreply.github.com>
2021-06-23 16:34:49 -0400

    Add tests for get_cumul_miscoverage_rates

--- tests/testthat/test-misc.R
+++ tests/testthat/test-misc.R
@@ -24,3 +24,35 @@ test_that("naive_confidence_intervals works", {
 
   expect_equal(ci_final_time$l, mean(x) - qnorm(1-alpha/2) / sqrt(n))
 })
+
+test_that("get_cumul_miscoverage_rate works", {
+  n <- 1000
+  num_repeats <- 500
+  var <- 10
+  alpha <- 0.1
+  miscoverage_rate <-
+    get_cumul_miscoverage_rate(
+      data_generator_fn = function(){rnorm(n, sd = sqrt(var))},
+      conf_set_fn = function(x){asymptotic_confseq(x,
+                                                   t_opt = 10,
+                                                   alpha = alpha,
+                                                   var = var)},
+      times = 1:n,
+      num_repeats = num_repeats,
+      mu = 0, n_cores = 1)
+
+  # Check to make sure that miscoverage is nondecreasing
+  expect_true(all(miscoverage_rate - c(0, miscoverage_rate[0:(n-1)]) >= 0))
+
+  # Check to make sure miscoverage is nonzero at the final time.
+  # This doesn't *always* need to be true, but in almost all cases, it will.
+  expect_true(miscoverage_rate[n] > 0)
+
+  # Calculate confidence interval for miscoverage rate.
+  # This test will fail at most 1% of the time.
+  coverage_test <- binom.test(miscoverage_rate[num_repeats] * num_repeats,
+                              n = num_repeats,
+                              p = alpha, alternative = "greater",
+                              conf.level = 0.99)
+  expect_lt(coverage_test$conf.int[1], alpha)
+})
