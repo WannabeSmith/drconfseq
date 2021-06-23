9f72fd5a6aabf04291ab04a933b3352127c00da8
Ian Waudby-Smith <5490890+WannabeSmith@users.noreply.github.com>
2021-06-23 13:52:18 -0400

    Add tests for std_conjmix_margin failing

--- tests/testthat/test-asymptotic_confseq.R
+++ tests/testthat/test-asymptotic_confseq.R
@@ -1,2 +1,8 @@
-test_that("", {
+test_that("std_conjmix_margin fails reasonably", {
+  expect_error(std_conjmix_margin(t = 10:1000, rho2 = 0, alpha = 0.05))
+  expect_error(std_conjmix_margin(t = c(-1, 10:1000), rho2 = 0.1, alpha = 0.05))
+  expect_error(std_conjmix_margin(t = 10:1000, rho2 = 0.1, alpha = 0))
+  expect_error(std_conjmix_margin(t = 10:1000, rho2 = 0.1, alpha = 1))
+  expect_silent(std_conjmix_margin(t = 10:1000, rho2 = 0.1, alpha = 0.05))
 })
+
