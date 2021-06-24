314789c277fccdf0ec16f6b834a383ce0805d888
Ian Waudby-Smith <5490890+WannabeSmith@users.noreply.github.com>
2021-06-24 09:06:53 -0400

    Add some tests for cs_ate and influence functions.
    * Largely focused on the sample splitting being correct, since that is a
    large potential source of error.

--- tests/testthat/test-doubly_robust.R
+++ tests/testthat/test-doubly_robust.R
@@ -6,26 +6,38 @@ test_that("pseudo_outcome_sequential sample splitting works", {
   treatment <- rbinom(n = n, size = 1, prob = 1/2)
   times <- 1:n
 
-  train_idx = rbinom(n, 1, 1/2)
-  manual_sample_split_cs <-
-    c(pseudo_outcome_sequential(y,
+  train_idx = sample(c(rep(1, 0.4*n), rep(0, 0.6*n)), replace = FALSE)
+
+  # Create pseudo-outcomes using the above train_idx split
+  pseudo_outcomes_trainsplit <-
+    pseudo_outcome_sequential(y,
                               X = as.data.frame(matrix(rnorm(n*1), ncol=1)),
                               treatment = treatment,
                               regression_fn_1 = function(y, X, newX){0},
                               propensity_score_fn = function(y, X, newX){1/2},
                               train_idx = train_idx,
                               cross_fit = FALSE,
-                              times = 1:n)[[n]],
-      pseudo_outcome_sequential(y,
+                              times = 1:n)
+  # Create the pseudo-outcomes using the opposite of the above train_idx split
+  pseudo_outcomes_evalsplit <-
+    pseudo_outcome_sequential(y,
                               X = as.data.frame(matrix(rnorm(n*1), ncol=1)),
                               treatment = treatment,
                               regression_fn_1 = function(y, X, newX){0},
                               propensity_score_fn = function(y, X, newX){1/2},
                               train_idx = 1-train_idx,
                               cross_fit = FALSE,
-                              times = 1:n)[[n]])
+                              times = 1:n)
 
-  auto_sample_split_cs <-
+  # pseudo_outcomes_trainsplit at the final time should have exactly the number
+  # of *evaluation* split subjects.
+  expect_equal(length(pseudo_outcomes_trainsplit[[n]]), sum(1-train_idx))
+  # Opposite should be true for pseudo_outcomes_eval.
+  expect_equal(length(pseudo_outcomes_evalsplit[[n]]), sum(train_idx))
+
+  # Create pseudo-outcomes using the above
+  # train_idx split butwith cross-fitting
+  auto_sample_splits <-
     pseudo_outcome_sequential(y,
                               X = as.data.frame(matrix(rnorm(n*1), ncol=1)),
                               treatment = treatment,
@@ -33,7 +45,19 @@ test_that("pseudo_outcome_sequential sample splitting works", {
                               propensity_score_fn = function(y, X, newX){1/2},
                               train_idx = train_idx,
                               cross_fit = TRUE,
-                              times = 1:n)[[n]]
+                              times = 1:n)
 
-  expect_equal(manual_sample_split_cs, auto_sample_split_cs)
+  auto_sample_split_final <- auto_sample_splits[[n]]
+
+  manual_sample_split_final <-
+    c(pseudo_outcomes_trainsplit[[n]], pseudo_outcomes_evalsplit[[n]])
+
+  # Splitting manually and concatenating training and evaluation sets should be
+  # equivalent to performing the sample split automatically.
+  expect_equal(manual_sample_split_final, auto_sample_split_final)
+
+  lengths <- unname(unlist(purrr::map(auto_sample_splits, length)))
+  # At time t, the cross-fit set
+  # should have exactly t pseudooutcomes.
+  expect_equal(lengths, 1:n)
 })
